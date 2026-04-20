406ec6371dad828ca317e0598e7684b71bbf64c8
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2026-04-20 10:41:28 +0700

    menambahkan file usercontroller di dalam folder controller

---
+++ src/test/java/com/example/pertemuan7/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.example.pertemuan7.controller;
+
+import com.example.pertemuan7.model.User;
+import com.example.pertemuan7.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody User request) {
+        userService.addUser(request);
+        return "user created successfully";
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+        return "user deleted successfully";
+    }
+
+    @PutMapping("/{id}")
+    public String updateUser(@PathVariable String id, @RequestBody User request){
+        userService.updateUser(id, request);
+        return "user updated successfully";
+    }
+}
